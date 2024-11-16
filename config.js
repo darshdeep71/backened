3c91a9d736af9409a521cf1c787bdf0c1cb470db
darshdeep singh <darsh@gmail.com>
2024-11-16 23:42:02 +0530

    first

---
+++ config.js
@@ -0,0 +1,11 @@
+let mongoose = require("mongoose");
+
+let atlass = 'mongodb+srv://test-yt:test123@test.yr3cp.mongodb.net/?retryWrites=true&w=majority&appName=test';
+
+mongoose.connect(atlass).then( ()=>{
+    console.log("connected")
+} ).catch( ()=>{
+    console.log("111")
+} );
+
+// mongoose.connect('mongodb://localhost:27017/live');
